3a56515bf611cf31420ef772b27cc4b932aa66ee
Estelope <elopez14567@gmail.com>
2023-07-19 00:00:54 -0600

    add recent kata challenges

--- Js/scripts.js
+++ Js/scripts.js
@@ -3,12 +3,62 @@
 
 function strCount(str, letter) {
   let count = 0
-  for(let i = 0; i<str.length; i++){
-  if (str[i] ===letter) {
-    count++;
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] === letter) {
+      count++;
+    }
+
+
   }
+  return count;
+}
+// Get the Middle Character 7kyu
 
+function getMiddle(str) {
+  const middleIndex = Math.floor(str.length / 2);
+
+  if (str.length % 2 === 1) {
+    // For odd-length strings, return a single middle character
+    return str[middleIndex];
+  } else {
+    // For even-length strings, return two middle characters
+    return str[middleIndex - 1] + str[middleIndex];
+  }
+}
+
+//refactor
+function getMiddle(str) {
+  let middle = Math.floor(str.length / 2);
+
+  if (str.length % 2 === 1) {
+    return str[middle];
+  }
+  else if (str.length % 2 === 0) {
+    return str[middle - 1] + str[middle];
+  }
+}
+
+//unscrabmled eggs
+
+function unscrambleEggs(word) {
+  return word.replace(/egg/g, "")
 
 }
-return count;
+
+//FIND ERROR it was simple syntax prob
+function main(verb, noun) {
+  return verb + noun;
 }
+
+//Complete the function which takes two arguments and returns all numbers which are divisible by the given divisor. First argument is an array of numbers and the second is the divisor.
+function divisibleBy(numbers, divisor) {
+  return numbers.filter(numbers => numbers % divisor === 0);
+
+}
+//uses filter() method on array to filter out numbers divisble by divisor with no remaninder 
+
+
+// finds area if sides are equal or perimiter if not
+const areaOrPerimeter = function(l , w) {
+  return l == w ? l*w : 2*(l + w)
+};
