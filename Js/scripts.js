0feff331ae873170fe255987e15820be3488fc4f
Estelope <elopez14567@gmail.com>
2023-08-09 00:21:50 -0600

    old people 8kyu

--- Js/scripts.js
+++ Js/scripts.js
@@ -116,3 +116,14 @@ function removeEveryOther(arr){
   
   }
   
+
+  function peopleWithAgeDrink(old) {
+    if (old < 14){
+    return "drink toddy"}
+      else if (old < 18){
+    return "drink coke"}
+      else if(old < 21){
+    return "drink beer"}
+      else if (old >= 21){
+    return "drink whisky"}
+    };
